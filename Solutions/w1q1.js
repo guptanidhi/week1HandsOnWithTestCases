d48a04941359ebab95c613e6a816ef4d3c5e1f49
guptanidhi <nidhi.gupta2484@gmail.com>
2017-06-20 14:53:13 +0530

    first solution was wrong so corrected it

--- Solutions/w1q1.js
+++ Solutions/w1q1.js
@@ -2,8 +2,9 @@
 	your machine and display its contents on the console.
 */
 
-var fs = require('fs'), filename = "data.txt";
-fs.readFile(filename, 'utf8', function(err, data) {
-  if (err) throw err;
-  console.log(data)
-});
+const name1 = "Nidhi";
+const companyName1 = "NIIT";
+const designation1 = "Associate Tech Lead";
+let output = "My name is "+name1+". I work in "+companyName1+" and my Designation is "+designation1;
+console.log(output);
+module.exports = output;
